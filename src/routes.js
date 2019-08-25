f795c95a8f53e5dad3c12ce96d222729d6eaa5b7
Diogo Machado <diogomachado_8@hotmail.com>
2019-08-25 07:26:03 -0300

    feat:Auth user #2 and Register User #1

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import authMiddleware from './app/middlewares/auth';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => res.send('ok'));
+
+routes.post('/session', SessionController.store);
+
+routes.post('/users', UserController.store);
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
