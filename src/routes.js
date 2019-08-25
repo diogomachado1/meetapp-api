23e585139a474534de738dbc0d1aa40aa8e68914
Diogo Machado <diogomachado_8@hotmail.com>
2019-08-25 13:01:57 -0300

    feat:add Meetup

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import MeetupController from './app/controllers/MeetupController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -17,6 +18,11 @@ routes.post('/session', SessionController.store);
 routes.post('/users', UserController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+routes.get('/meetup', MeetupController.index);
+routes.post('/meetup', MeetupController.store);
+routes.put('/meetup/:id', MeetupController.update);
+routes.delete('/meetup/:id', MeetupController.delete);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
